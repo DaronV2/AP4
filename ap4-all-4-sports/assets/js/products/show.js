e865229544dac82d635e9cccda83787a65a3a024
Daron <darkenciel31@gmail.com>
2025-03-10 16:14:13 +0100

    create ajax request to add a product to cart

--- ap4-all-4-sports/assets/js/products/show.js
+++ ap4-all-4-sports/assets/js/products/show.js
@@ -1,8 +1,12 @@
 const elements = {
-    minusButton : document.getElementById('minusButton'),
-    plusButton : document.getElementById('plusButton'),
-    quantityInput : document.getElementById('inputCart'),
+    minusButton: document.getElementById('minusButton'),
+    plusButton: document.getElementById('plusButton'),
+    quantityInput: document.getElementById('inputCart'),
+    cart: document.getElementById('addToCart'),
+    idProduct: document.getElementById('idProduct')
 };
+
+
 
 elements.minusButton.addEventListener("click", () => {
     if (elements.quantityInput.value > 1) {
@@ -11,7 +15,18 @@ elements.minusButton.addEventListener("click", () => {
 });
 
 elements.plusButton.addEventListener("click", () => {
-    if(elements.quantityInput.value < 99) {
+    if (elements.quantityInput.value < 99) {
         elements.quantityInput.value++;
     }
 });
+
+elements.cart.addEventListener("click", async () => {
+    await fetch("/ajax/add", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "X-Requested-With": "XMLHttpRequest"
+        },
+        body: JSON.stringify({ quantity: elements.quantityInput.value, idProduct: elements.idProduct.value ? elements.idProduct.value : "" })
+    }).then(response => response.json())
+})
