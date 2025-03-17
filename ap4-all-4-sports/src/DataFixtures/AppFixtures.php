5651445e5192be25a0cdfb21aeae0ce7466fa0f1
Daron <darkenciel31@gmail.com>
2025-03-17 18:49:31 +0100

    Update fixtures

--- ap4-all-4-sports/src/DataFixtures/AppFixtures.php
+++ ap4-all-4-sports/src/DataFixtures/AppFixtures.php
@@ -2,13 +2,21 @@
 
 namespace App\DataFixtures;
 
+use Aisle;
 use App\Entity\Fournisseur;
+use App\Entity\IsOrdered;
+use App\Entity\Order;
 use App\Entity\Pictures;
 use App\Entity\Products;
 use App\Entity\Rayon;
+use App\Entity\Stocke;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\User;
+use App\Entity\Warehouse;
+use DateTime;
+use Modules;
+use Status;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
@@ -29,6 +37,7 @@ class AppFixtures extends Fixture
         $admin = new User();
         $admin->setEmail('admin@admin.com');
         $admin->setPassword($this->hasher->hashPassword($admin, 'admin'));
+        $admin->setRoles(['ROLE_ADMIN']);
         $manager->persist($admin);
 
         $listRayons = ['FootBall','BasketBall','Tennis','Golf'];
@@ -57,7 +66,6 @@ class AppFixtures extends Fixture
         $produit->setDescription('Ballon de foot taille 5');
         $produit->setRating(5);
         $manager->persist($produit);
-
         $pic = new Pictures();
         $pic->setUrl('https://pim.decapro.com/MEDIA/MEDIA/LARGE/1da/1da8d14b-0f55-4205-8994-ad663edb2e12.png');
         $pic->setIdProduit($produit);
@@ -71,11 +79,50 @@ class AppFixtures extends Fixture
         $produitTwo->setDescription('Ballon de tennis taille 32 cm de diamètre');
         $produitTwo->setRating(4);
         $manager->persist($produitTwo);
-
         $picTwo = new Pictures();
         $picTwo->setUrl('https://contents.mediadecathlon.com/p2430275/k$1e8d3c7513161416388576796c87a0f3/sq/balles-de-tennis-australian-open4-jaune-polyvalente.jpg?format=auto&f=800x0');
         $picTwo->setIdProduit($produitTwo);
         $manager->persist($picTwo);
+
+        $listEntrepots = ["Valenciennes","Paris"];
+        $listEntrepotsObj = [];
+        foreach ($listEntrepots as $entrepot){
+            $ware = new Warehouse();
+            $ware->setCity($entrepot);
+            $listEntrepotsObj[] = $ware;
+            $manager->persist($ware);
+        }
+
+        $stock = new Stocke();
+        $stock->setReferenceProduct($produit);
+        $stock->setIdWarehouse($listEntrepotsObj[0]);
+        $stock->setModule(Modules::M1->value);
+        $stock->setAislse(Aisle::E->value);
+        $stock->setRowWarehouse("12");
+        $stock->setSection("8");
+        $manager->persist($stock);
+
+        $order = new Order();
+        $order->setDateOrder(new DateTime());
+        $prods = [$produit, $produitTwo];
+        $totalPrice = 0.0;
+        foreach($prods as $prod){
+            $order->addReferenceProduct($prod);
+            $totalPrice =+ $prod->getPriceTtc();
+        }
+        $order->setTotalPrice($totalPrice);
+        $order->setEmailClient($admin->getEmail());
+        $order->setStatus(Status::expediated->value);
+        $manager->persist($order);
+
+        foreach($prods as $prod){
+            $commande = new IsOrdered();
+            $commande->setCommand($order);
+            $commande->setProduct($prod);
+            $commande->setQuantity(20);
+            $manager->persist($commande);
+        }    
+
 
         $manager->flush();
     }
