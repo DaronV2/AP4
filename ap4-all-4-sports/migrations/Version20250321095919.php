683db5040f70dcf624b547031978625ad8e7922b
Daron <darkenciel31@gmail.com>
2025-03-21 11:14:27 +0100

    Clean migrations

--- ap4-all-4-sports/migrations/Version20250321095919.php
+++ ap4-all-4-sports/migrations/Version20250321095919.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250127144857 extends AbstractMigration
+final class Version20250321095919 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -22,22 +22,26 @@ final class Version20250127144857 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE address (id INT AUTO_INCREMENT NOT NULL, city VARCHAR(255) NOT NULL, street VARCHAR(255) NOT NULL, postal_code VARCHAR(5) NOT NULL, number VARCHAR(255) NOT NULL, country VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE fournisseur (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE `order` (id INT AUTO_INCREMENT NOT NULL, email_client_id INT NOT NULL, date_order DATETIME NOT NULL, total_price DOUBLE PRECISION NOT NULL, quantite INT NOT NULL, INDEX IDX_F529939816E0F60A (email_client_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE is_ordered (id INT AUTO_INCREMENT NOT NULL, command_id INT NOT NULL, product_id INT NOT NULL, quantity INT NOT NULL, UNIQUE INDEX UNIQ_D52E995133E1689A (command_id), UNIQUE INDEX UNIQ_D52E99514584665A (product_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `order` (id INT AUTO_INCREMENT NOT NULL, email_client_id INT NOT NULL, status_id INT NOT NULL, date_order DATETIME NOT NULL, total_price DOUBLE PRECISION NOT NULL, INDEX IDX_F529939816E0F60A (email_client_id), INDEX IDX_F52993986BF700BD (status_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE order_products (order_id INT NOT NULL, products_id INT NOT NULL, INDEX IDX_5242B8EB8D9F6D38 (order_id), INDEX IDX_5242B8EB6C8A81A9 (products_id), PRIMARY KEY(order_id, products_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE pictures (id INT AUTO_INCREMENT NOT NULL, id_produit_id INT NOT NULL, url VARCHAR(255) NOT NULL, INDEX IDX_8F7C2FC0AABEFE2C (id_produit_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE products (id INT AUTO_INCREMENT NOT NULL, id_fournisseur_id INT NOT NULL, id_rayon_id INT NOT NULL, price_ttc DOUBLE PRECISION NOT NULL, description VARCHAR(255) NOT NULL, INDEX IDX_B3BA5A5A5A6AC879 (id_fournisseur_id), INDEX IDX_B3BA5A5A83FBC2C7 (id_rayon_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE products (id INT AUTO_INCREMENT NOT NULL, fournisseur_id INT NOT NULL, rayon_id INT NOT NULL, price_ttc DOUBLE PRECISION NOT NULL, description VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, rating INT NOT NULL, code_product VARCHAR(255) NOT NULL, INDEX IDX_B3BA5A5A670C757F (fournisseur_id), INDEX IDX_B3BA5A5AD3202E52 (rayon_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE rayon (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE status (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE stocke (id INT AUTO_INCREMENT NOT NULL, reference_product_id INT DEFAULT NULL, id_warehouse_id INT DEFAULT NULL, quantity INT NOT NULL, INDEX IDX_A2232D54119A5315 (reference_product_id), INDEX IDX_A2232D54734199AC (id_warehouse_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE stocke (id INT AUTO_INCREMENT NOT NULL, reference_product_id INT DEFAULT NULL, id_warehouse_id INT DEFAULT NULL, quantity INT NOT NULL, module VARCHAR(255) NOT NULL, aislse VARCHAR(255) NOT NULL, row_warehouse VARCHAR(255) NOT NULL, section VARCHAR(255) NOT NULL, INDEX IDX_A2232D54119A5315 (reference_product_id), INDEX IDX_A2232D54734199AC (id_warehouse_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE warehouse (id INT AUTO_INCREMENT NOT NULL, city VARCHAR(255) NOT NULL, module VARCHAR(255) NOT NULL, aisle VARCHAR(255) NOT NULL, row_warehouse VARCHAR(255) NOT NULL, section INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE warehouse (id INT AUTO_INCREMENT NOT NULL, city VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE is_ordered ADD CONSTRAINT FK_D52E995133E1689A FOREIGN KEY (command_id) REFERENCES `order` (id)');
+        $this->addSql('ALTER TABLE is_ordered ADD CONSTRAINT FK_D52E99514584665A FOREIGN KEY (product_id) REFERENCES products (id)');
         $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_F529939816E0F60A FOREIGN KEY (email_client_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_F52993986BF700BD FOREIGN KEY (status_id) REFERENCES status (id)');
         $this->addSql('ALTER TABLE order_products ADD CONSTRAINT FK_5242B8EB8D9F6D38 FOREIGN KEY (order_id) REFERENCES `order` (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE order_products ADD CONSTRAINT FK_5242B8EB6C8A81A9 FOREIGN KEY (products_id) REFERENCES products (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE pictures ADD CONSTRAINT FK_8F7C2FC0AABEFE2C FOREIGN KEY (id_produit_id) REFERENCES products (id)');
-        $this->addSql('ALTER TABLE products ADD CONSTRAINT FK_B3BA5A5A5A6AC879 FOREIGN KEY (id_fournisseur_id) REFERENCES fournisseur (id)');
-        $this->addSql('ALTER TABLE products ADD CONSTRAINT FK_B3BA5A5A83FBC2C7 FOREIGN KEY (id_rayon_id) REFERENCES rayon (id)');
+        $this->addSql('ALTER TABLE products ADD CONSTRAINT FK_B3BA5A5A670C757F FOREIGN KEY (fournisseur_id) REFERENCES fournisseur (id)');
+        $this->addSql('ALTER TABLE products ADD CONSTRAINT FK_B3BA5A5AD3202E52 FOREIGN KEY (rayon_id) REFERENCES rayon (id)');
         $this->addSql('ALTER TABLE stocke ADD CONSTRAINT FK_A2232D54119A5315 FOREIGN KEY (reference_product_id) REFERENCES products (id)');
         $this->addSql('ALTER TABLE stocke ADD CONSTRAINT FK_A2232D54734199AC FOREIGN KEY (id_warehouse_id) REFERENCES warehouse (id)');
     }
@@ -45,16 +49,20 @@ final class Version20250127144857 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE is_ordered DROP FOREIGN KEY FK_D52E995133E1689A');
+        $this->addSql('ALTER TABLE is_ordered DROP FOREIGN KEY FK_D52E99514584665A');
         $this->addSql('ALTER TABLE `order` DROP FOREIGN KEY FK_F529939816E0F60A');
+        $this->addSql('ALTER TABLE `order` DROP FOREIGN KEY FK_F52993986BF700BD');
         $this->addSql('ALTER TABLE order_products DROP FOREIGN KEY FK_5242B8EB8D9F6D38');
         $this->addSql('ALTER TABLE order_products DROP FOREIGN KEY FK_5242B8EB6C8A81A9');
         $this->addSql('ALTER TABLE pictures DROP FOREIGN KEY FK_8F7C2FC0AABEFE2C');
-        $this->addSql('ALTER TABLE products DROP FOREIGN KEY FK_B3BA5A5A5A6AC879');
-        $this->addSql('ALTER TABLE products DROP FOREIGN KEY FK_B3BA5A5A83FBC2C7');
+        $this->addSql('ALTER TABLE products DROP FOREIGN KEY FK_B3BA5A5A670C757F');
+        $this->addSql('ALTER TABLE products DROP FOREIGN KEY FK_B3BA5A5AD3202E52');
         $this->addSql('ALTER TABLE stocke DROP FOREIGN KEY FK_A2232D54119A5315');
         $this->addSql('ALTER TABLE stocke DROP FOREIGN KEY FK_A2232D54734199AC');
         $this->addSql('DROP TABLE address');
         $this->addSql('DROP TABLE fournisseur');
+        $this->addSql('DROP TABLE is_ordered');
         $this->addSql('DROP TABLE `order`');
         $this->addSql('DROP TABLE order_products');
         $this->addSql('DROP TABLE pictures');
