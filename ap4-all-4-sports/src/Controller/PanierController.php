fad1be48c20149bd448049913212561f28329cd8
Yghermaoui <youcef18.ghermaoui@gmail.com>
2025-04-27 20:09:16 +0200

    git merge master

--- ap4-all-4-sports/src/Controller/PanierController.php
+++ ap4-all-4-sports/src/Controller/PanierController.php
@@ -1,20 +1,51 @@
 <?php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+use App\Entity\Products;
+use App\Repository\ProductsRepository;
 
 class PanierController extends AbstractController
 {
     #[Route('/panier', name: 'panier_index')]
-    public function index(SessionInterface $session): Response
+    public function index(SessionInterface $session, Request $request, SerializerInterface $serializer, ProductsRepository $prodrepo): Response
     {
-        $panier = $session->get('panier', ['items' => [], 'total' => 0]);
+        // Récupérer la session
+        $session = $request->getSession();
+        $cart = $session->get('cart', []);
 
+        // Initialiser un tableau pour stocker les produits
+        $productsString = [];
+
+        // Boucler sur les éléments du panier
+      /*  foreach ($cart as $key => $item) {
+            // Décoder le JSON stocké dans la valeur du tableau
+            $decodedItem = json_decode($item, true); 
+
+            // Vérifier si le produit est bien présent et l'ajouter au tableau
+            if (isset($decodedItem['product'])) {
+                // $decodedToObj = $serializer->deserialize($decodedItem['product'], Products::class, 'json');
+                $decodedItem['product']['id'] = $key;
+                $productsString[] = json_encode($decodedItem['product']); // Ajout avec un retour à la ligne
+            }
+        }
+        $produitsObjet = [];
+        // Debug pour voir les produits extraits
+        foreach($productsString as $produit){
+            $produitsObjet[] = $serializer->deserialize($produit, Products::class, 'json');
+        }
+
+        // dd($productsString);
+        dd($serializer->serialize($prodrepo->findOneBy(['id' => 2]) ,'json'));
+*/
         return $this->render('panier/panier.html.twig', [
-            'panier' => $panier
+            'products' => NULL
         ]);
     }
 }
